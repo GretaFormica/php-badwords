c4e3d7b364d68d87854d6b9b90a73fc6c8039e54
GretaFormica <greta.formica@gmail.com>
2023-03-09 16:05:32 +0100

    php

--- file.php
+++ file.php
@@ -1,12 +1,28 @@
+<?php
+    $_GET["words"]. " " .$_GET["bad"];
+    $_words = $_GET["words"];
+    $_bad = $_GET["bad"];
+
+    $_correzione = str_replace($_bad, "***", $_words);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>badwords</title>
+    <title>badwords result</title>
 </head>
 <body>
+
+    <h1>
+        risultato: 
+        <?php
+            echo $_correzione;
+        ?>
+        
+    </h1>
     
 </body>
 </html>
